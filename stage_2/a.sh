94a8395be5c6f2d2b10bd472065984a4e68bb5ba
Rashmi Mech <rashmimech18@gmail.com>
2015-06-23 02:31:48 +0530

    nightly backup

--- stage_2/a.sh
+++ stage_2/a.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#latex actualpresent_ras1.tex
+#dvips -o actualpresent_ras1.ps actualpresent_ras1
+#ps2pdf actualpresent_ras1.ps
 latex actualpresent_ras.tex
 dvips -o actualpresent_ras.ps actualpresent_ras
 ps2pdf actualpresent_ras.ps
---
+++ stage_2/a.sh~
@@ -0,0 +1,5 @@
+#!/bin/bash
+latex actualpresent_ras.tex
+dvips -o actualpresent_ras.ps actualpresent_ras
+ps2pdf actualpresent_ras.ps
+#evince actualpresent_ras.pdf
